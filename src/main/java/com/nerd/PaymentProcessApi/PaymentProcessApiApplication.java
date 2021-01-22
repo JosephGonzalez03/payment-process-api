5213a2413a77490926b9f8c35577d624392008bb
JosephGonzalez03 <josephdg3@gmail.com>
2021-01-21 20:24:32 -0800

    feat: Add PaymentProcessApiApplication.java

---
+++ src/main/java/com/nerd/PaymentProcessApi/PaymentProcessApiApplication.java
@@ -0,0 +1,13 @@
+package com.nerd.paymentprocessapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentProcessApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentProcessApiApplication.class, args);
+	}
+
+}
