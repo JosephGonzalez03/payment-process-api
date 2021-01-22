d9dda9386c8eb87dffe0a7e2098842c4e65462dc
JosephGonzalez03 <josephdg3@gmail.com>
2021-01-21 20:23:22 -0800

    feat: Add Spring starter project

---
+++ src/test/java/com/nerd/paymentprocessapi/PaymentProcessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nerd.paymentprocessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentProcessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
